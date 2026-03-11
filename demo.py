1732733aecaf9c2247afa6d2d6fff777e57faf11
ofekeliyahu <Ofek.Eliyahu@gmail.com>
2026-03-11 12:40:12 +0200

    fix

--- demo.py
+++ demo.py
@@ -2,17 +2,13 @@ import random
 
 def main():
     print("Welcome to the Innonation Day!")
-    print_random_number()
 
 
 
 
 
-def print_random_number():
-    print(generate_random_number())
 
-def generate_random_number():
-    return random.randint(1, 100)
+
 
 if __name__ == "__main__":
     main()
