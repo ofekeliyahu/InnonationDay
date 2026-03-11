d9e7f24330fcdad3cd00621bb5359c620875660b
ofekeliyahu <Ofek.Eliyahu@gmail.com>
2026-03-11 11:20:52 +0200

    added func

--- demo.py
+++ demo.py
@@ -1,5 +1,11 @@
+import random
+
 def main():
-    print("Hello, World!")
+    print("Welcome to the Innonation Day!")
+
+
+def generate_random_number():
+    return random.randint(1, 100)
 
 if __name__ == "__main__":
     main()
